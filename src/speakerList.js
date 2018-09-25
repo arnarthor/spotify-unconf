c32a1209744ccdd983d27a4349d88a2dede90fc3
Arnar Thor Sveinsson <arnar.th92@gmail.com>
2018-09-25 12:26:02 +0200

    minor updatse

--- src/speakerList.js
+++ src/speakerList.js
@@ -6,7 +6,7 @@ export default [
       'https://pbs.twimg.com/profile_images/1042305371782742016/pqAS-w1w_400x400.jpg'
   },
   {
-    name: 'Arnar',
+    name: 'Arnar Thor',
     talk: 'Writing React in Reason',
     image:
       'https://communityfund.imgix.net/b5614162-0b9b-473d-ab4f-96f8bdf59d50.png?fit=crop&w=500&h=500'
